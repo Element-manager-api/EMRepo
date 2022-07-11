374eb1b5d4e895f078303f2b9d739abc7edf6718
Element-manager-api <80879988+Element-manager-api@users.noreply.github.com>
2022-07-11 22:54:37 +0530

    Delete Extensions/Agent Browser UI Extensions directory

--- Extensions/Agent Browser UI Extensions/DecimalExtension/src/alert.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../libs/jquery/index.d.ts"/>
-
-$(() => {
-  const urlToParse = new URL(window.location.href);
-  const alertMessage = urlToParse.searchParams.get('alertMessage');
-  $('#alertMessage').html(alertMessage);
-});
