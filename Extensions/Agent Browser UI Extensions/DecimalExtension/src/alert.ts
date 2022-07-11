05accdf4b195064593a0f6e6bf0990590186bd90
Element-manager-api <vishnu.saseendrababu@oracle.com>
2022-07-11 16:55:56 +0000

    JenkinsSearchPackage Export. Build :40

---
+++ Extensions/Agent Browser UI Extensions/DecimalExtension/src/alert.ts
@@ -0,0 +1,7 @@
+/// <reference path="../libs/jquery/index.d.ts"/>
+
+$(() => {
+  const urlToParse = new URL(window.location.href);
+  const alertMessage = urlToParse.searchParams.get('alertMessage');
+  $('#alertMessage').html(alertMessage);
+});
